79237dd1aaca99aae91aa0e3d0a6e9261b965566
Nancy jha <nancyjha364@gmail.com>
2026-01-14 13:37:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let spotsLeft = 10; 
+
+
+const modal = document.getElementById("modal");
+const mainBtn = document.getElementById("mainBtn");
+const counterEl = document.getElementById("counter");
+const form = document.getElementById("regForm");
+
+mainBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+
+
+function closeModal() {
+    modal.classList.add("hidden");
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+  
+    if (spotsLeft > 0) {
+        spotsLeft--;     
+        counterEl.innerText = spotsLeft;
+        alert("Success! You are registered.");
+    } else {
+        alert("Sorry, no spots left!");
+    }
+
+
+    closeModal();
+});
